17fbe37e58fa1ee7843595c25f92dc3e0b48bf50
Blade12629 <blade1234@web.de>
2020-08-08 17:43:46 +0200

    Moved embed command from GAFBot to Skybot

---
+++ DiscordCommands/EmbedCommand.cs
@@ -0,0 +1,267 @@
+﻿using DSharpPlus.Entities;
+using SkyBot;
+using SkyBot.Discord.CommandSystem;
+using SkyBot.Discord.Data;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+using System.Text;
+
+namespace DiscordCommands
+{
+    public class EmbedCommand : ICommand
+    {
+
+
+        public bool IsDisabled { get; set; }
+
+        public string Command => "embed";
+
+        public CommandType CommandType => CommandType.Public;
+        public string Description => "Create, edit or reverse embeds";
+
+        public string Usage =>  "```" + Environment.NewLine +
+                                "!embed create channelId embedCode/-url:www.example.com/raw/text" + Environment.NewLine +
+                                "!embed edit channelId messageId embedCode/-url:www.example.com/raw/text" + Environment.NewLine +
+                                "!embed reverse channelId messageId" + Environment.NewLine +
+                                "```";
+
+        AccessLevel ICommand.AccessLevel => throw new NotImplementedException();
+
+        public void Invoke(CommandHandler handler, CommandEventArg args)
+        {
+            try
+            {
+                HelpCommand.ShowHelp(args.Channel, this);
+                if (args.Guild == null)
+                {
+                    args.Channel.SendMessageAsync("This command is only usable in a discord channel");
+                    return;
+                }
+                else if (args.Parameters.Count == 0)
+                {
+                    HelpCommand.ShowHelp(args.Channel, this);
+                    return;
+                }
+
+                StringBuilder sbuilder = new StringBuilder(args.Parameters[0]);
+
+                for (int i = 1; i < args.Parameters.Count; i++)
+                    sbuilder.Append(" " + args.Parameters[i]);
+
+                string afterCMD = sbuilder.ToString();
+
+                string download = null;
+
+                int urlStart = afterCMD.IndexOf("-url:");
+                string urlString = null;
+                if (urlStart > 0)
+                {
+                    urlString = afterCMD.Remove(0, urlStart + 5);
+                    if (!string.IsNullOrEmpty(urlString))
+                    {
+                        using (WebClient wc = new WebClient())
+                            download = wc.DownloadString(urlString);
+                    }
+                }
+
+
+                string @params = afterCMD;
+
+                if (urlString != null && download != null)
+                    @params = @params.Replace("-url:" + urlString, download, StringComparison.CurrentCultureIgnoreCase);
+
+                int index = @params.IndexOf(' ', StringComparison.CurrentCultureIgnoreCase);
+                if (index <= 1)
+                {
+                    HelpCommand.ShowHelp(args.Channel, this);
+                    return;
+                }
+
+                string cmdType = @params.Substring(0, index).TrimStart(' ').TrimEnd(' ').ToLower(CultureInfo.CurrentCulture);
+                @params = @params.Remove(0, index + 1);
+
+                string channelIdStr;
+                ulong channelId;
+                ulong messageId;
+                DiscordChannel dchannel;
+                DiscordMessage dmessage;
+                EmbedJson embedJson;
+                DiscordEmbed embed;
+                if (cmdType.Equals("edit", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    index = @params.IndexOf(' ', StringComparison.CurrentCultureIgnoreCase);
+                    if (index <= 1)
+                    {
+                        HelpCommand.ShowHelp(args.Channel, this);
+                        return;
+                    }
+
+                    channelIdStr = @params.Substring(0, index);
+
+                    @params = @params.Remove(0, index + 1);
+
+                    if (!ulong.TryParse(channelIdStr, out channelId))
+                    {
+                        args.Channel.SendMessageAsync("Could not parse channel id: " + channelIdStr);
+                        return;
+                    }
+
+                    index = @params.IndexOf(' ', StringComparison.CurrentCultureIgnoreCase);
+                    if (index <= 1)
+                    {
+                        HelpCommand.ShowHelp(args.Channel, this);
+                        return;
+                    }
+
+                    string messageIdStr = @params.Substring(0, index);
+                    @params = @params.Remove(0, index + 1);
+
+                    if (!ulong.TryParse(messageIdStr, out messageId))
+                    {
+                        args.Channel.SendMessageAsync("Could not parse message id: " + messageIdStr);
+                        return;
+                    }
+
+                    dchannel = Program.DiscordHandler.Client.GetChannelAsync(channelId).ConfigureAwait(false).GetAwaiter().GetResult();
+                    dmessage = dchannel.GetMessageAsync(messageId).ConfigureAwait(false).GetAwaiter().GetResult();
+
+                    if (dmessage == null)
+                    {
+                        args.Channel.SendMessageAsync("Could not find message " + messageId);
+                        return;
+                    }
+
+                    embedJson = Newtonsoft.Json.JsonConvert.DeserializeObject<EmbedJson>(@params);
+
+                    if (embedJson == null)
+                    {
+                        args.Channel.SendMessageAsync("Failed to parse your embed json");
+                        return;
+                    }
+
+                    embed = embedJson.BuildEmbed();
+                    dmessage.ModifyAsync(embedJson.content ?? default(Optional<string>), embed);
+
+                    return;
+                }
+                else if (cmdType.Equals("reverse", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    index = @params.IndexOf(' ', StringComparison.CurrentCultureIgnoreCase);
+                    if (index <= 1)
+                    {
+                        HelpCommand.ShowHelp(args.Channel, this);
+                        return;
+                    }
+
+                    channelIdStr = @params.Substring(0, index);
+                    @params = @params.Remove(0, index + 1);
+
+                    if (!ulong.TryParse(channelIdStr, out channelId))
+                    {
+                        args.Channel.SendMessageAsync("Could not parse channel id: " + channelIdStr);
+                        return;
+                    }
+
+                    string messageIdStr = @params;
+
+                    if (!ulong.TryParse(messageIdStr, out messageId))
+                    {
+                        args.Channel.SendMessageAsync("Could not parse message id: " + messageIdStr);
+                        return;
+                    }
+                    dchannel = Program.DiscordHandler.Client.GetChannelAsync(channelId).ConfigureAwait(false).GetAwaiter().GetResult();
+                    dmessage = dchannel.GetMessageAsync(messageId).ConfigureAwait(false).GetAwaiter().GetResult();
+
+                    if (dmessage == null)
+                    {
+                        args.Channel.SendMessageAsync("Could not find message " + messageId);
+                        return;
+                    }
+
+                    var reversed = EmbedJson.ReverseEmbed(dmessage);
+                    string json = Newtonsoft.Json.JsonConvert.SerializeObject(reversed, Newtonsoft.Json.Formatting.Indented);
+
+                    args.Channel.SendMessageAsync("```js" + Environment.NewLine + json + Environment.NewLine + "```");
+                    return;
+                }
+
+                index = @params.IndexOf(' ', StringComparison.CurrentCultureIgnoreCase);
+                if (index <= 1)
+                {
+                    HelpCommand.ShowHelp(args.Channel, this);
+                    return;
+                }
+
+                channelIdStr = @params.Substring(0, index);
+                @params = @params.Remove(0, index + 1);
+
+                if (@params.Length <= 1)
+                {
+                    HelpCommand.ShowHelp(args.Channel, this);
+                    return;
+                }
+
+                if (!ulong.TryParse(channelIdStr, out channelId))
+                {
+                    args.Channel.SendMessageAsync("Could not parse channel id: " + channelIdStr);
+                    return;
+                }
+
+                Logger.Log(channelIdStr + " : " + channelId);
+
+                dchannel = Program.DiscordHandler.Client.GetChannelAsync(channelId).ConfigureAwait(false).GetAwaiter().GetResult();
+                embedJson = Newtonsoft.Json.JsonConvert.DeserializeObject<EmbedJson>(@params);
+
+                if (embedJson == null)
+                {
+                    args.Channel.SendMessageAsync("Failed to parse your embed json");
+                    return;
+                }
+
+                embed = embedJson.BuildEmbed();
+                dchannel.SendMessageAsync(embedJson.content, false, embed);
+            }
+#pragma warning disable CA1031 // Do not catch general exception types
+            catch (Exception ex)
+#pragma warning restore CA1031 // Do not catch general exception types
+            {
+                Logger.Log(ex.ToString(), LogLevel.Error);
+            }
+        }
+
+        private ulong GetChannelId(string channel)
+        {
+            int indexStart = channel.IndexOf('<', StringComparison.CurrentCulture);
+
+            if (indexStart == -1)
+                return 0;
+
+            string id = channel.Remove(0, indexStart + 1);
+
+            if (!id[0].Equals('#'))
+                return 0;
+
+            id = id.Remove(0, 1);
+            indexStart = channel.IndexOf('>', StringComparison.CurrentCulture);
+            if (indexStart == -1)
+                return 0;
+            try
+            {
+                id = id.Substring(0, indexStart - 1);
+            }
+#pragma warning disable CA1031 // Do not catch general exception types
+            catch (Exception)
+#pragma warning restore CA1031 // Do not catch general exception types
+            {
+                id = id.Substring(0, indexStart - 2);
+            }
+
+            if (ulong.TryParse(id, out ulong result))
+                return result;
+
+            return 0;
+        }
+    }
+}
---
+++ SkyBot/Discord/Data/EmbedJson.cs
@@ -0,0 +1,187 @@
+﻿using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkyBot.Discord.Data
+{
+    public class EmbedJson
+    {
+        public string content { get; set; }
+        public Embed embed { get; set; }
+
+        public static EmbedJson ReverseEmbed(DiscordMessage message)
+        {
+            if (message == null)
+                return null;
+
+            DiscordEmbed embed = message.Embeds.ElementAt(0);
+
+            Thumbnail thumbnail = null;
+            Image image = null;
+
+            Author author = null;
+            Footer footer = null;
+
+            if (embed.Thumbnail != null)
+                thumbnail = new Thumbnail()
+                {
+                    url = embed.Thumbnail.Url.ToString()
+                };
+
+            if (embed.Image != null)
+                image = new Image()
+                {
+                    url = embed.Image.Url.ToString()
+                };
+
+            if (embed.Author != null)
+                author = new Author()
+                {
+                    url = embed.Author.Url.ToString(),
+                    icon_url = embed.Author.IconUrl.ToString(),
+                    name = embed.Author.Name
+                };
+
+            if (embed.Footer != null)
+                footer = new Footer()
+                {
+                    icon_url = embed.Footer.IconUrl?.ToString() ?? null,
+                    text = embed.Footer.Text
+                };
+
+            List<Field> fields = null;
+
+            if (embed.Fields != null)
+            {
+                fields = new List<Field>();
+
+                foreach (DiscordEmbedField f in embed.Fields)
+                    fields.Add(new Field()
+                    {
+                        inline = f.Inline,
+                        value = f.Value,
+                        name = f.Name
+                    });
+            }
+
+            EmbedJson result = new EmbedJson()
+            {
+                content = message.Content,
+                embed = new Embed()
+                {
+                    author = author,
+                    thumbnail = thumbnail,
+                    image = image,
+                    footer = footer,
+                    color = embed.Color.Value,
+                    description = embed.Description,
+                    fields = fields?.ToArray() ?? null,
+                    timestamp = embed.Timestamp?.DateTime ?? DateTime.MinValue,
+                    title = embed.Title,
+                    url = embed.Url?.ToString() ?? null
+                }
+            };
+
+            return result;
+        }
+
+        public DiscordEmbed BuildEmbed()
+        {
+            Thumbnail thumbnail = embed.thumbnail;
+            Image image = embed.image;
+
+            Author author = embed.author;
+            Footer footer = embed.footer;
+
+            DiscordEmbedBuilder builder = new DiscordEmbedBuilder()
+            {
+                Title = embed.title,
+                Description = embed.description,
+                Url = embed.url,
+                Color = new DiscordColor(embed.color),
+                Timestamp = embed.timestamp,
+                ThumbnailUrl = thumbnail?.url ?? null,
+                ImageUrl = image?.url ?? null,
+            };
+
+            if (footer != null)
+            {
+                builder.Footer = new DiscordEmbedBuilder.EmbedFooter()
+                {
+                    IconUrl = footer?.icon_url ?? null,
+                    Text = footer?.text ?? null,
+                };
+            }
+
+            if (author != null)
+            {
+                builder.Author = new DiscordEmbedBuilder.EmbedAuthor()
+                {
+                    IconUrl = author?.icon_url ?? null,
+                    Name = author?.name ?? null,
+                    Url = author?.url ?? null
+                };
+            }
+
+            if (embed.fields != null)
+            {
+                foreach (Field f in embed.fields)
+                    builder.AddField(f.name, f.value, f?.inline ?? false);
+            }
+
+            return builder.Build();
+        }
+    }
+
+#pragma warning disable CA1707 // Identifiers should not contain underscores
+#pragma warning disable CA1819 // Properties should not return arrays
+#pragma warning disable CA1056 // Uri properties should not be strings
+    public class Embed
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string url { get; set; }
+        public int color { get; set; }
+        public DateTime timestamp { get; set; }
+        public Footer footer { get; set; }
+        public Thumbnail thumbnail { get; set; }
+        public Image image { get; set; }
+        public Author author { get; set; }
+        public Field[] fields { get; set; }
+    }
+
+    public class Footer
+    {
+        public string icon_url { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Thumbnail
+    {
+        public string url { get; set; }
+    }
+
+    public class Image
+    {
+        public string url { get; set; }
+    }
+
+    public class Author
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public string icon_url { get; set; }
+    }
+
+    public class Field
+    {
+        public string name { get; set; }
+        public string value { get; set; }
+        public bool inline { get; set; }
+    }
+#pragma warning restore CA1707 // Identifiers should not contain underscores
+#pragma warning restore CA1819 // Properties should not return arrays
+#pragma warning restore CA1056 // Uri properties should not be strings
+}
